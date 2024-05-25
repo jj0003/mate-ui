16115e49169d9732c8d7ea3df7cfdb912bcf1359
jan furio <janfurio@tutanota.com>
2024-05-25 21:36:48 +1000

    refactor: Update Footer component layout and styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,13 +4,8 @@ import { cva, VariantProps } from "class-variance-authority";
 import { Button } from "@/components/Button";
 
 const footerStyles = cva([
-  "bg-gray-400",
-  "text-white",
-  "py-4",
-  "flex",
-  "justify-between",
-  "items-center",
-  "px-4",
+  "bg-white",
+  "p-10",
 ], {
   variants: {
     layout: {
@@ -28,28 +23,49 @@ type FooterProps = ComponentProps<"footer"> & VariantProps<typeof footerStyles>;
 export const Footer = ({ layout, className, ...props }: FooterProps) => {
   return (
     <footer className={cn(footerStyles({ layout, className }))} {...props}>
-      <div className="">
-        <h1 className="text-2xl  font-bold">MateUI</h1>
-        <p>© 2024 MateUI. All rights reserved.</p>
-      </div>
-      <div className="flex flex-col items-start">
-            <Button variant={'underline'}>Home</Button>
-            <Button variant={'underline'}>About</Button>
-            <Button variant={'underline'}>Services</Button>
-            <Button variant={'underline'}>Contact</Button>
-      </div>
-
-      <div className="mt-4 md:mt-0 flex">
-        <a href="#" className="text-[#18181B] mr-4">
-          <svg /* SVG for social media icon */></svg>
-        </a>
-        <a href="#" className="text-[#18181B] mr-4">
-          <svg /* SVG for social media icon */></svg>
-        </a>
-        <a href="#" className="text-[#18181B]">
-          <svg /* SVG for social media icon */></svg>
-        </a>
-      </div>
+        <div className="flex lg:flex-row flex-col place-content-evenly">
+            <div className="flex flex-col ">
+                {/* Title */}
+                <h1 className="text-3xl text-black font-bold italic">
+                    MateUI
+                </h1>
+                <p className='text-black'>
+                    Engineered with &lt;3 for Productivity.
+                </p>
+            </div>
+            <div className="flex flex-col items-center lg:mt-0 mt-10">
+                {/* Subtitle */}
+                <p className="text-gray-600 mb-2 uppercase tracking-widest">
+                    Get Started
+                </p>
+                <Button variant={'underline'}>Link</Button>
+                <Button variant={'underline'}>Link</Button>
+                <Button variant={'underline'}>Link</Button>
+            </div>
+            <div className="flex flex-col items-center lg:mt-0 mt-10">
+                {/* Subtitle */}
+                <p className="text-gray-600 mb-2 uppercase tracking-widest">
+                    Download
+                </p>
+                <Button variant={'underline'}>Link</Button>
+                <Button variant={'underline'}>Link</Button>
+                <Button variant={'underline'}>Link</Button>
+            </div>
+            <div className="flex flex-col items-center lg:mt-0 mt-10">
+                {/* Subtitle */}
+                <p className="text-gray-600 mb-2 uppercase tracking-widest">
+                    our team
+                </p>
+                <Button variant={'underline'}>Link</Button>
+                <Button variant={'underline'}>Link</Button>
+                <Button variant={'underline'}>Link</Button>
+            </div>
+        </div>
+        <div className="flex flex-col items-center">
+            <p className="text-gray-600 mt-10 uppercase tracking-widest">
+                ©MateUI 2024
+            </p>
+        </div>
     </footer>
   );
 };
